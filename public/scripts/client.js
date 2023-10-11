a331a13ad37cd54ea0165720eb138ebc030f3990
Alexander Da Costa <bsmuse@tutamail.com>
2023-10-11 06:39:36 -0600

    Finish styling of main and article (tweet) styling

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,5 +2,19 @@
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+ */ 
 
+$(document).ready(function() {
+  $("#tweet-text").on('input', function() {
+    const textLength = $(this).val().length;
+    const remainingCharacters = 140 - textLength;
+    $('.counter').text(remainingCharacters);
+
+    if (remainingCharacters < 0) {
+      $('.counter').addClass('red');
+    } else  if (remainingCharacters > 0) {
+      $('.counter').removeClass('red');
+    }
+  });
+});
+ 
